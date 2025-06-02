04ad491bc9d9a1bc295ac43a7a1cc5c0cc1b330b
Eshag5526 <eshag5526@gmail.com>
2025-06-02 08:04:51 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/eshaonjava/App.java
@@ -0,0 +1,13 @@
+package com.eshaonjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
